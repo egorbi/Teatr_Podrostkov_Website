29cdfbd57a2797bdc07c9717b97ca514b2775868
egorbi <egorbi2005@gmail.com>
2025-01-31 17:23:26 +0300

    added afisha page & routes in navbar

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
@@ -14,3 +14,11 @@ def logout():
 @auth.route('/sign-up')
 def sign_up():
     return '<p>Sign up</p>'
+
+@auth.route('/afisha')
+def afisha():
+    return render_template("afisha.html")
+
+@auth.route('/home')
+def home():
+    return render_template("home.html")
--- website/views.py
+++ website/views.py
@@ -7,4 +7,6 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
+def afisha():
+    return render_template("afisha.html")
 
